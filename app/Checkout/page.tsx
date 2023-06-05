727fb76b69394a7b4897ea91c54563aab97b9976
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2023-06-05 20:56:37 +0500

    deployed to vercel

--- app/Checkout/page.tsx
+++ app/Checkout/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 import Stripe from "stripe";
 
-const stripe=new Stripe(`sk_test_51NFHRIL9ITLbiKbQVq4TaRCe0RFw2CjSRdP7QGY9eYNWsZolMeKAZHI1MXxZcxc3dli3RQGi06YVLz5f2G3BfLkB00owUvy1zV`,{apiVersion:'2022-11-15'})
+//const stripe=new Stripe(``,{apiVersion:'2022-11-15'})
 
 export default function Checkout(){
     return(
         <div className=" text-center">
-            <div className=" text-3xl text-white font-semibold">Checkout</div>
+            <div className=" text-3xl text-white font-semibold">We only accept cash on delivery</div>
         </div>
     )
 }
